74b8fd2924dc3984fb5b6d05eefaabd6cbb768ba
Suhrob Yusubakhmedov <torsontorfinn@gmail.com>
2025-06-28 23:59:20 +0500

    implement delta-frame, next add thresholding

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
-    check, frame = video.read()
-    cv2.imshow("Builtin Cam Video", frame)
+    check, frame = video.read()  
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gauss = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gauss
+    
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gauss)
+    cv2.imshow("Builtin Cam Video", delta_frame)
+    
+    print(delta_frame)
 
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
 
-video.release()
+video.release() 

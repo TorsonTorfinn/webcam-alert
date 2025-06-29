52746e83e0743ec41fb699ba4b2e05a9a298aaee
Suhrob Yusubakhmedov <torsontorfinn@gmail.com>
2025-06-29 22:47:35 +0500

    Threshold detecting and rectangle appending implemented by contourArea, etc.

---
+++ emailing.py
@@ -0,0 +1,3 @@
+
+def send_email():
+    print('Email was Sent()joke')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+from emailing import send_email
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -15,12 +17,26 @@ while True:
         first_frame = gray_frame_gauss
     
     delta_frame = cv2.absdiff(first_frame, gray_frame_gauss)
-    cv2.imshow("Builtin Cam Video", delta_frame)
+    threshold_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dilate_frame = cv2.dilate(threshold_frame, None, iterations=2)
+
+    cv2.imshow('Builtin Cam Video', dilate_frame)
     
-    print(delta_frame)
+    contours, check = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
+        if rectangle.any():
+            send_email()
+
+    cv2.imshow('Video', frame)
 
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
 
-video.release() 
+video.release()

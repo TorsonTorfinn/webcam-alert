4b80278afcd7aa18d1dc03e4f6d2cdf4b090eff5
Suhrob Yusubakhmedov <torsontorfinn@gmail.com>
2025-07-13 21:59:32 +0500

    impl auto emailing function

--- emailing.py
+++ emailing.py
@@ -1,3 +1,32 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+import imghdr
 
-def send_email():
-    print('Email was Sent()joke')
+load_dotenv(dotenv_path='.env')
+
+PASSWORD = os.getenv('google_app_password')
+SENDER = os.getenv('sender_gmail')
+RECEIVER = os.getenv('receiver_gmail')
+
+
+def send_mail(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Object was detected around"
+    email_message.set_content("Hey, move on & rm all stuff, smbd is coming in.")
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)   
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == '__main__':
+    send_mail(image_path='images/kitty.png')

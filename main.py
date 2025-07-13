f76fe47686715484e721de536e48319c1aeb47df
Suhrob Yusubakhmedov <torsontorfinn@gmail.com>
2025-07-13 20:52:06 +0500

    capturing image with object

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import time
 from emailing import send_email
+import glob
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+img_count = 1
 
 while True:
     status = 0 
@@ -33,6 +36,11 @@ while True:
         
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/img_{img_count}.png", frame)
+            img_count = img_count + 1
+            all_images = glob.glob('images/*.png')
+            index = int(len(all_images) / 2)
+            image_with_obj = all_images[index]
     
     status_list.append(status) # 0 - no object && 1 - there's object
 
@@ -47,42 +55,3 @@ while True:
         break
 
 video.release()
-
-
-# video = cv2.VideoCapture(0)
-# time.sleep(1)
-
-# first_frame = None
-
-# while True:
-#     check, frame = video.read()  
-#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     gray_frame_gauss = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-
-#     if first_frame is None:
-#         first_frame = gray_frame_gauss
-    
-#     delta_frame = cv2.absdiff(first_frame, gray_frame_gauss)
-#     threshold_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
-#     dilate_frame = cv2.dilate(threshold_frame, None, iterations=2)
-
-#     cv2.imshow('Builtin Cam Video', dilate_frame)
-    
-#     contours, check = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#     for contour in contours:
-#         if cv2.contourArea(contour) < 5000:
-#             continue
-#         x, y, w, h = cv2.boundingRect(contour)
-#         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        
-#         if rectangle.any():
-#             send_email()
-
-#     cv2.imshow('Video', frame)
-
-#     key = cv2.waitKey(1)
-#     if key == ord("q"):
-#         break
-
-# video.release()
